332f3be63e545c83fed9cd013648dc1dad05f366
Guanngxu <446678850@qq.com>
2025-10-13 12:54:45 +0800

    修改 4644 欠压保护

--- pages/4644/uvlo/uvlo.js
+++ pages/4644/uvlo/uvlo.js
@@ -1,28 +1,25 @@
 Page({
   data: {
     VStart: 4.5,
-    VStop: 4,
+    VStopResult: 4.10,
 
-    R1Result: 10.00,
+    R1: 10.00,
     R2Result: 3.35,
   },
 
   compute_click() {
     const VStart = Number(this.data.VStart);
-    const VStop = Number(this.data.VStop);
-
-
-    const R1Result = 10;
+    const R1 = Number(this.data.R1);
+    
     const Vrunrise = 1.13;
     const Vrundown = 1.03;
     // Vrundown 的典型值为 1.03
     // Vrunrise 的典型值为 1.13
-    // const R2Result = (Vrundown * R1Result) / (VStop - Vrundown);
-    const R2Result = Vrunrise * 10 / (VStart - Vrunrise);
-    // console.info((R1Result + R2Result) / R2Result * Vrundown);
+    const R2Result = Vrunrise * R1 / (VStart - Vrunrise);
+    const VStopResult = (R2Result + R1) / R2Result * Vrundown;
 
     this.setData({
-      R1Result: R1Result.toFixed(2),
+      VStopResult: VStopResult.toFixed(2),
       R2Result: R2Result.toFixed(2),
     })
   },
